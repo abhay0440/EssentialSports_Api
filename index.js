a8d6f8a09df81ab045a438518106d844d83d37d0
Abhay.Dahibhale <int.abhay.dahiphale@xpressbees.com>
2023-05-16 19:42:13 +0530

    api 5

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// const express = require('express');
-// const axios = require('axios');
-// const cors = require('cors');
-
-// const app = express();
-// const port = 3001; // The port your proxy server will run on
-
-// app.use(cors());
-
-// app.get('/rss', async (req, res) => {
-//   try {
-//     const response = await axios.get('https://www.essentiallysports.com/feed/');
-//     res.send(response.data);
-//   } catch (error) {
-//     res.status(500).send('Error fetching RSS feed');
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Proxy server listening on port ${port}`);
-// });
-
-
-
-
-// index.js
 const express = require('express')
 
 const app = express()
