60d64f7c878a8357841f6900f844855df594fd6a
Abhay.Dahibhale <int.abhay.dahiphale@xpressbees.com>
2023-05-16 19:48:12 +0530

    api 6

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express()
 const PORT = 4000
@@ -7,9 +9,19 @@ app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+app.use(cors());
+
+app.get('/rss', async (req, res) => {
+  try {
+    console.log("//////////////")
+    const response = await axios.get('https://www.essentiallysports.com/feed/');
+    res.send(response.data);
+  } catch (error) {
+    console.log(error,"====");
+    res.status(500).send('Error fetching RSS feed');
+  }
+});
+
 
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
